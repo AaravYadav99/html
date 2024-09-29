ab7d7fd5a5a5d841d7370243446538012ce501b7
Aarav Yadav <kumaryadavanjani@gmail.com>
2024-09-29 20:34:04 +0545

    python class 2

---
+++ website part1/python/classworl.py
@@ -0,0 +1,9 @@
+name="codingal"
+num =100
+print("name=",name)
+print("number=",num)
+
+res=input("enter the name")
+print("name=",res)
+n=int(input("enter a number"))
+print("num=",n)
--- website part1/python/first.py
+++ website part1/python/first.py
@@ -1,3 +1,9 @@
-print ("welcome to python")
+print ("welcome to python programming")
+print(2323)
+a=10
+print(a)
+print ("a=",10,end=" ")
 
+print("name \nAddress")
+print("name \tAddress")
 
