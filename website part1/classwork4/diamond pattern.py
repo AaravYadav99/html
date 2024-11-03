07d332e71d38cea193b0f7c39d216719cee47f87
Aarav Yadav <kumaryadavanjani@gmail.com>
2024-11-03 20:21:27 +0545

    classwork

--- website part1/classwork4/diamond pattern.py
+++ website part1/classwork4/diamond pattern.py
@@ -1,12 +1,11 @@
 def print_number_diamond(n):
-    # Top half of the diamond
+  
     for i in range(1, n + 1):
         print(" " * (n - i) + " ".join(str(j) for j in range(1, i + 1)))
 
-    # Bottom half of the diamond
     for i in range(n - 1, 0, -1):
         print(" " * (n - i) + " ".join(str(j) for j in range(1, i + 1)))
 
-# Get user input for the size of the diamond
+
 n = int(input("Enter the size of the diamond: "))
 print_number_diamond(n)
