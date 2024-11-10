36b9119d77e184ca21a68252d61e113304f95a25
Aarav Yadav <kumaryadavanjani@gmail.com>
2024-11-10 20:39:57 +0545

    classwork5

---
+++ website part1/classwork6/wheather condition .py
@@ -0,0 +1,7 @@
+def weather_condition(season):
+    if season=="Autumn":
+        print("the weather is  medium in autumn")
+    elif season=="spring":
+        print("the weather is good in spring season")
+season=input("type your season")
+weather_condition(season)
