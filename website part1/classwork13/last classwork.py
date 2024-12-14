557dee6cb54c306119a349518a76c2565d6a9956
Aarav Yadav <kumaryadavanjani@gmail.com>
2024-12-14 20:32:49 +0545

    classwork13

---
+++ website part1/classwork13/last classwork.py
@@ -0,0 +1,8 @@
+country_code ={
+    'Nepal':'0097',
+    'India':'0091',
+    'USA':'001',
+    'Qatar':'0974'
+}
+print("country_code for Nepal",country_code.get("Nepal","not found"))
+print("country_code for UK",country_code.get("UK","not found"))
