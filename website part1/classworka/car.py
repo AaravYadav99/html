d9c8cb162de854529710c249625adb35ff6f633f
Aarav Yadav <kumaryadavanjani@gmail.com>
2024-12-28 20:04:11 +0545

    classs

---
+++ website part1/classworka/car.py
@@ -0,0 +1,11 @@
+class vehicle:
+    def __init__(self,maxspeed,mileage):
+        self.maxspeed=maxspeed
+        self.mileage=mileage
+car=vehicle(400,32)
+print("the maxpseed of car is",car.maxspeed)
+print("the mileage of car is",car.mileage)
+bike=vehicle(320,18)
+print("the maxpseed of bike is",bike.maxspeed)
+print("the mileage of bike is",bike.mileage)
+
---
+++ website part1/classworka/studenr.py
@@ -0,0 +1,6 @@
+class student:
+    grade=7
+    print("hi,i am a student of grade",grade)
+student1=student()
+student2=student()
+print(student.grade)
