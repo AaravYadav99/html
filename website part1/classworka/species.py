55b070895ed3022100bc1b2aaab127dacd50278f
Aarav Yadav <kumaryadavanjani@gmail.com>
2024-12-28 20:23:34 +0545

    classs

---
+++ website part1/classworka/species.py
@@ -0,0 +1,17 @@
+class parrot:
+    species=("bird")
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+maccaw=parrot("jack",2)
+lory=parrot("bob",3)
+print("the species of maccaw is",maccaw.species)
+print("the species of maccaw is",maccaw.name)
+print("the species of maccaw is",maccaw.age)
+
+print("the species of lory is",lory .species)
+print("the species of lory  is",lory .name)
+print("the species of lory  is",lory .age)
+
+
+
